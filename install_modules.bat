f072a4928cf421f1b8a62de9356eb929d5903f01
QBitor <nicolasdouard@gmail.com>
2017-03-22 20:50:23 +0100

    Testing

--- install_modules.bat
+++ install_modules.bat
@@ -1,3 +1,12 @@
 sudo pip3 install pathlib
 sudo pip3 install nanpy
 sudo pip3 install paramiko
+
+#navio only
+git clone https://github.com/emlid/Navio2.git
+cd Navio2
+cd Python
+sudo python setup.py install
+
+
+
--- navio_client/rc_data_client.py
+++ navio_client/rc_data_client.py
@@ -8,26 +8,28 @@ import navio.util
 host = "localhost"
 port = 15555
 
-try:
-	#navio defs
-	navio.util.check_apm()
-	rcin = navio.rcinput.RCInput()
+#try:
+#navio defs
+#navio.util.check_apm()
+rcin = navio.rcinput.RCInput()
+
+#open socket
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((host, port))
+print('Connection on {}'.format(port))
+
+#TODO: set not running behavior 
+running = True
+while running:
+	#read period
+	#corresponds to Aux1 bound to RKnob
+	period = rcin.read(5)
+	print(period)
+	#send period
+	socket.sendall(bytes(period, encoding="ascii"))
+	time.sleep(1)
+socket.close()
+print("Closing socket...")
 	
-	#open socket
-	socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	socket.connect((host, port))
-	print('Connection on {}'.format(port))
-	
-	#TODO: set not running behavior 
-	running = True
-	while running:
-		#read period
-		period = rcin.read(2)
-		#send period
-		socket.sendall(bytes(period, encoding="ascii"))
-		time.sleep(1)
-	socket.close()
-	print("Closing socket...")
-	
-except:
-	print('Cound not send data to master board server. Is networking properly configured?')
+#except:
+#	print('Cound not send data to master board server. Is networking properly configured?')
