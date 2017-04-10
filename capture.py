714d3f71f7a9047508f82eb8529d68f663c00155
QBitor <nicolasdouard@gmail.com>
2017-04-10 12:47:20 +0200

    Fix

--- capture.py
+++ capture.py
@@ -60,14 +60,17 @@ def parse_logger_info():
 def start_logger_app(ip, username, password, ssh_run_command, duration, destination):
 	final_command = ssh_run_command + ' --duration ' + duration + ' --destination \'' + destination + '\''
 	print('Start command: ' + final_command)
-	print('Will now attempt to start capture over SSH...')
+	
 	#try:
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.connect(ip, username=username, password=password)
+	
+	final_command = 'while true; do ' + final_command + ' && break; done'
+	print('Will run: ' + final_command)
+	print('Will now attempt to start capture over SSH...')
 	ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('mkdir testdir')
 	
-	stdout=ssh_stdout.readlines()
 	#except:
 	#sys.exit('Could not start capture over SSH.')
 	
