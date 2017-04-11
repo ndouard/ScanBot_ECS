6b106226d0a495b35e59ac65fd72faf1c2908918
QBitor <nicolasdouard@gmail.com>
2017-04-11 11:39:47 +0200

    Fix

--- Turret.py
+++ Turret.py
@@ -120,8 +120,7 @@ class Turret:
 		#for input in range 1097-1894, coef is 0.229 and 1097 should be substracted
 		if(1090 < pwm_input < 1900):
 			if((last_pwm_input - 20) <= pwm_input <= (last_pwm_input + 20)):
-				#print('Not updated')
-				
+				print('Not updated')	
 			else:
 				Turret.servo_pan.write((pwm_input - 1097) * 0.229)
 		else:
